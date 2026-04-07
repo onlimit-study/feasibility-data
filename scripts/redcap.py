fbf5f6d747462965c8e0799bbc6ccdcf572f5978
martonvago <57952344+martonvago@users.noreply.github.com>
2026-04-07 08:25:56 +0100

    feat: ✨ download REDCap data dictionary script (#33)
    # Description
    
    This PR adds the ability to download the data dict from REDCap.
    
    Closes #24 
    
    This PR needs an in-depth review.
    
    ## Checklist
    
    - [x] Formatted Markdown
    - [x] Ran `just run-all`
    
    ---------
    
    Co-authored-by: Marton Vago <mvago@fe-open-01.gdk>
    Co-authored-by: Luke W. Johnston <lwjohnst@gmail.com>

---
+++ scripts/redcap.py
@@ -0,0 +1,38 @@
+import json
+import os
+from pathlib import Path
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+def get_data_dict_from_redcap() -> list[dict[str, str]]:
+    """Gets the data dictionary from REDCap."""
+    token = os.environ.get("REDC_CPH_API_KEY")
+    if not token:
+        raise RuntimeError("REDC_CPH_API_KEY environment variable is not set.")
+
+    data = {
+        "token": token,
+        "content": "metadata",
+        "format": "json",
+        "returnFormat": "json",
+    }
+    response = requests.post("https://redcap.regionh.dk/api/", data=data, timeout=30)
+    response.raise_for_status()
+    return response.json()
+
+
+def save_data_dict():
+    """Saves the data dictionary from REDCap to `scripts/data_dictionary.json`."""
+    data_dict = get_data_dict_from_redcap()
+    file_path = Path("scripts") / "data_dictionary.json"
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(file_path, "w") as f:
+        json.dump(data_dict, f, indent=2, ensure_ascii=False)
+
+
+if __name__ == "__main__":
+    save_data_dict()
