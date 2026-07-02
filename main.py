e93382bfb44a03a5ad1e7b53dad468ccb00dfc59
Marton Vago <marci.vago@gmail.com>
2026-07-02 18:31:50 +0100

    revert: ⏪️ add back main.py

---
+++ main.py
@@ -0,0 +1,43 @@
+# import polars as pl
+import seedcase_sprout as sp
+
+from src.feasibility_data.package_properties import package_properties
+
+
+def main():
+    """Run the build pipeline of the data package."""
+    ## PROPERTIES
+
+    ## Create the properties file in default location if it doesn't already exist.
+    # sp.create_properties_script()
+
+    # package_path = sp.PackagePath()
+
+    ## Save the properties to `datapackage.json`.
+    sp.write_properties(properties=package_properties)
+
+    ## BATCH DATA
+
+    ## Save the batch data.
+    # sp.write_resource_batch(
+    #     data=raw_data, resource_properties=resource_properties
+    # )
+
+    ## RESOURCE DATA
+
+    ## Read in all the batch data files for the resource as a list.
+    # batch_data = sp.read_resource_batches(
+    #     resource_properties=resource_properties
+    # )
+    ## Join them all together into a single Polars DataFrame.
+    # joined_data = sp.join_resource_batches(
+    #     data_list=batch_data, resource_properties=resource_properties
+    # )
+    ## Write the resource data file.
+    # sp.write_resource_data(
+    #     data=joined_data, resource_properties=resource_properties
+    # )
+
+
+if __name__ == "__main__":
+    main()
