8c73cefea6711fd40daf68d250f33cb065fadf03
Marton Vago <marci.vago@gmail.com>
2026-08-05 14:14:33 +0100

    refactor: ♻️ small tweaks

--- src/feasibility_data/data/redcap/core.py
+++ src/feasibility_data/data/redcap/core.py
@@ -21,26 +21,25 @@ def split_forms(
     form_to_events = _get_form_event_mapping(event_metadata)
     repeating_form_names = _get_repeating_forms(repeating_forms)
 
-    for df in _create_dfs_for_forms(
+    for df in _split_data_by_form(
         data, form_to_fields, form_to_events, repeating_form_names
     ):
         _write_df(df, forms_dir, timestamp)
 
 
-def _create_dfs_for_forms(
+def _split_data_by_form(
     raw_df: pl.DataFrame,
     form_to_fields: dict[str, list[str]],
     form_to_events: dict[str, list[str]],
     repeating_form_names: set[str],
 ) -> list[pl.DataFrame]:
-    """Transforms the raw data into one dataframe per form."""
-    dfs = []
-    for form_entry in form_to_fields.items():
-        dfs.append(
-            _create_df_for_form(
-                form_entry, raw_df, form_to_events, repeating_form_names
-            )
-        )
+    """Split the raw data into one dataframe per form."""
+    dfs = so.fmap(
+        form_to_fields.items(),
+        lambda form_entry: _create_df_for_form(
+            form_entry, raw_df, form_to_events, repeating_form_names
+        ),
+    )
     return so.keep(dfs, lambda df: not df.is_empty())
 
 
@@ -63,85 +62,6 @@ def _create_df_for_form(
     is_repeating = form_name in repeating_form_names
     content_fields = so.keep(field_names, lambda field: field not in REDCAP_ID_COLS)
 
-    # TODO: just for testing, remove!
-    content_fields = so.keep(
-        content_fields,
-        lambda field: (
-            field
-            not in [
-                "home_situation_notes_v0",
-                "exclusion_reason_v0",
-                "excluded_note_wfv0",
-                "is_randomised",
-                "isnot_randomised",
-                "consent_24h_reflection_date_info_v1",
-                "screening_exclusion_v1",
-                "hba1c_exclusion_v1",
-                "screening_done_v1",
-                "mysp_guidance_v1",
-                "dropout_record_v1",
-                "bp_information_v2",
-                "information_before_long_visit_v5",
-                "information_adverse_events_v6",
-                "hba1c_normal_info_v6",
-                "hba1c_mildly_elevated_info_v6",
-                "hba1c_elevated_info_v6",
-                "information_myfood24_v7",
-                "information_before_long_visit_v7",
-                "information_adverse_events_v8",
-                "hba1c_normal_info_v8",
-                "hba1c_mildly_elevated_info_v8",
-                "hba1c_elevated_info_v8",
-                "information_myfood24_v9",
-                "information_before_long_visit_v9",
-                "information_adverse_events_v10",
-                "inform_project_doctor_cgm_v10",
-                "hba1c_normal_info_v10",
-                "hba1c_mildly_elevated_info_v10",
-                "hba1c_elevated_info_v10",
-                "inform_project_doctor_cgm_group_meeting_3",
-                "hba1c_normal_info_week_30",
-                "hba1c_mildly_elevated_info_week_30",
-                "hba1c_elevated_info_week_30",
-                "hba1c_normal_info_week_42",
-                "hba1c_mildly_elevated_info_week_42",
-                "hba1c_elevated_info_week_42",
-                "hba1c_normal_info_extra",
-                "hba1c_mildly_elevated_info_extra",
-                "hba1c_elevated_info_extra",
-                "ds_questionnaire",
-                "psqi_questionnaire",
-                "sf12_questionnaire",
-                "eq5d_questionnaire",
-                "tobac_questionnaire",
-                "sefnc_questionare",
-                "sefnc_questionare_v6",
-                "sefnc_questionare_v10",
-                "tils_questionnaire",
-                "bds_questionnaire_intro",
-                "yfas_questionnaire",
-                "ssfeh_questionnaire_intro",
-                "paid5_questionnaire",
-                "bed_q_instruction",
-            ]
-        ),
-    )
-
-    filters = [
-        # Keep only rows for events where the form was filled in
-        pl.col("redcap_event_name").is_in(events),
-        # Keep only non-empty rows
-        pl.any_horizontal(
-            so.fmap(content_fields, lambda field: pl.col(field).is_not_null())
-        ),
-    ]
-
-    if is_repeating:
-        # Repeating forms are listed in separate rows for each submission,
-        # not in the same row as data for other forms for the same event.
-        # Keep only rows for the form.
-        filters.append(pl.col("redcap_repeat_instrument") == form_name)
-
     columns = [
         pl.col("record_id_s").alias("participant_id"),
         pl.col("redcap_event_name").alias("event_id"),
@@ -153,11 +73,20 @@ def _create_df_for_form(
     ]
 
     if is_repeating:
-        # Different submissions for the same repeating form are told apart by
+        # Submissions for the same participant at the same event are told apart by
         # `redcap_repeat_instance`.
         columns.insert(
             2, pl.col("redcap_repeat_instance").cast(pl.String).alias("submission_id")
         )
+
+    filters = [
+        # Keep only rows coming from events where the form was filled in
+        pl.col("redcap_event_name").is_in(events),
+        # Keep only non-empty rows
+        pl.any_horizontal(
+            so.fmap(content_fields, lambda field: pl.col(field).is_not_null())
+        ),
+    ]
 
     return raw_df.filter(filters).select(columns)
 
@@ -165,7 +94,7 @@ def _create_df_for_form(
 def _get_form_field_mapping(
     field_metadata: list[dict[str, str]],
 ) -> dict[str, list[str]]:
-    """Gets a mapping from form name to a list of field names in that form."""
+    """Get a mapping from form name to a list of field names in that form."""
     mapping: dict[str, list[str]] = defaultdict(list)
     for field in field_metadata:
         mapping[field["form_name"]].append(field["field_name"])
@@ -176,7 +105,7 @@ def _get_form_field_mapping(
 def _get_form_event_mapping(
     event_metadata: list[dict[str, str]],
 ) -> dict[str, list[str]]:
-    """Creates a mapping from form names to event names where the form is filled in."""
+    """Get a mapping from form names to event names where the form is filled in."""
     mapping: dict[str, list[str]] = defaultdict(list)
     for item in event_metadata:
         mapping[item["form"]].append(item["unique_event_name"])
@@ -185,12 +114,12 @@ def _get_form_event_mapping(
 
 
 def _get_repeating_forms(repeating_forms: list[dict[str, str]]) -> set[str]:
-    """Gets a set of repeating form names."""
+    """Get the set of repeating form names."""
     return set(so.fmap(repeating_forms, itemgetter("form_name")))
 
 
 def _write_df(df: pl.DataFrame, forms_dir: Path, timestamp: str) -> None:
-    """Writes the dataframe."""
+    """Write the dataframe."""
     form_name = df["form_name"][0]
     file_path = forms_dir / timestamp / f"{form_name}.parquet"
     file_path.parent.mkdir(parents=True, exist_ok=True)
